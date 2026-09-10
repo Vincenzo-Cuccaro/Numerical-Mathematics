db21d97f63055b0e2a060cbfc7655602de59b35c
Vincenzo-Cuccaro <enzocuccaro03@gmail.com>
2026-09-10 14:03:08 +0200

    Replace minQuadratiDiscreto with leastSquares function

--- Least-squares-polynomial-interpolation/linearBenchmark.m
+++ Least-squares-polynomial-interpolation/linearBenchmark.m
@@ -3,7 +3,7 @@ x_nodes = [1   1    1    1    1 2   3    4   5 5   5]';
 y_nodes = [1.1 1.05 1.12 1.07 2 2.1 3.05 3.7 4 4.8 5.2]';
 
 x = linspace(0.5, 5.5, 101);
-p = minQuadratiDiscreto(x, x_nodes, y_nodes, n);
+p = leastSquares(x, x_nodes, y_nodes, n);
 
 plot(x', p');
 hold on
