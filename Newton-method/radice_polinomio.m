01cb3da1211dfeee9e9939f342cfefb23c35a168
Vincenzo-Cuccaro <enzocuccaro03@gmail.com>
2026-09-10 13:51:22 +0200

    Refactor Newton method functions for clarity

--- Newton-method/radice_polinomio.m
+++ Newton-method/radice_polinomio.m
@@ -5,5 +5,5 @@ q = @(x) (x - 2)*(x - 1)^2;
 dq = @(x) (x-1)^2 + 2*(x-2)*(x-1);
 
 x0 = 1.4;
-[xi, n] = newtonMultipleIgnaro(x0, 10^-5, q, dq)
-[xc, m] = newtonMultipleConscio(x0, 10^-5, q, dq, 2)
+[xi, n] = newtonMultipleUnknownOrder(x0, 10^-5, q, dq)
+[xc, m] = newtonMultipleKnownOrder(x0, 10^-5, q, dq, 2)
